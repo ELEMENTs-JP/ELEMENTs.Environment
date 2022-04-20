72893bb13472965657bec9774b9c9bc68b0b43bf
JP <juergenpott@googlemail.com>
2022-04-20 10:01:57 +0200

    - HTML Span added - ToSecureString Helper Method added

---
+++ ELEMENTS.Controls/00-Infrastructure/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELEMENTS
+{
+    public static class Helper
+    {
+        public static string ToSecureString(this object strg)
+        {
+            try
+            {
+                if (strg == null)
+                    return string.Empty;
+
+                return strg.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fail: " + ex.Message);
+                return string.Empty;
+            }
+        }
+    }
+}
