63a344f18d20b387f7b137eafcf9d0688925c05f
JP <juergenpott@googlemail.com>
2022-04-20 15:42:29 +0200

    - SELECT and Option added

---
+++ ELEMENTS.Controls/Selection/OptionItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELEMENTS
+{
+    public class OptionItem
+    {
+        public string ID { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public bool isSelected { get; set; } = false;
+    }
+}
