de5622719efd149c7df504b97a3cf5ea9650256d
JP <juergenpott@googlemail.com>
2022-04-20 18:11:34 +0200

    - KPI List added - Trend Indicator added - KPI Item added - KPI Panel added - example pages updated

---
+++ ELEMENTS.Controls/KPI/KpiList.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELEMENTS
+{
+    public partial class KpiListItem
+    {
+        public string KPI { get; set; } = "Umsatz";
+
+        public string Value { get; set; } = "100.000 EUR";
+    }
+}
