b5157374e0ad2394f04382317243bc327e71985e
JP <juergenpott@googlemail.com>
2022-04-20 21:11:17 +0200

    - Fast Login added - Log in added - Registration added - Permission List added - Change Password added - Request Access added - Request new Password added - Set new Password added - example pages updated

---
+++ ELEMENTS.Controls/Security/Classes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELEMENTS
+{
+    public class Permission
+    {
+        public string ID { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public bool Value { get; set; } = false;
+    }
+}
