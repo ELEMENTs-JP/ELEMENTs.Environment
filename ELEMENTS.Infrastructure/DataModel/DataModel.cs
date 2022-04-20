609a68d6ae81491db89b046f4b24a783138d718e
JP <juergenpott@googlemail.com>
2022-04-20 16:59:24 +0200

    - ItemsList added - ListItem added - pages updated

--- ELEMENTS.Infrastructure/DataModel/DataModel.cs
+++ ELEMENTS.Infrastructure/DataModel/DataModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ELEMENTS.Infrastructure
 {
@@ -48,6 +50,25 @@ namespace ELEMENTS.Infrastructure
         }
     }
 
+    //public class FileNotification
+    //{
+    //    public string FullFilePath { get; set; }
+    //    public Guid FileGUID { get; set; }
+    //    public string OriginalFileName { get; set; }
+    //}
+    //public class FileNotificationService
+    //{
+    //    public event Func<FileNotification, Task> Notify;
+
+    //    public void NotifySync(FileNotification notification)
+    //    {
+    //        if (Notify != null)
+    //        {
+    //            Notify.Invoke(notification);
+    //        }
+    //    }
+    //}
+
     public class FileNotification
     {
         public string FullFilePath { get; set; }
@@ -66,32 +87,165 @@ namespace ELEMENTS.Infrastructure
             }
         }
     }
-    public class ProductInfo 
+
+    public interface IFileUploadService
     {
-        /// <summary>
-        /// Gibt die Bezeichnung des Produktes zurück
-        /// Returns the title of the product
-        /// </summary>
-        public string Title { get; set; }
-
-        /// <summary>
-        /// Gibt das Label des Produktes zurück
-        /// Returns the maturity level of the product
-        /// </summary>
-        public string Label { get; set; }
-
-        /// <summary>
-        /// Gibt die ID des Herstellers des Produktes zurück
-        /// Returns the business id of the constructor of the product
-        /// </summary>
-        public string ID { get; set; }
-        
-        /// <summary>
-        /// Gibt die technische Versionsnummer des Produktes zurück
-        /// Returns the technical version number of the product
-        /// </summary>
-        public string Version { get; set; }
+        string GetContentRootPath();
+        Task UploadAsync(Microsoft.AspNetCore.Components.Forms.IBrowserFile file);
+        FileNotificationService NotificationService { get; }
     }
+
+    public class FileUploadService : IFileUploadService
+    {
+        // https://docs.microsoft.com/de-de/aspnet/core/blazor/file-uploads?view=aspnetcore-6.0&pivots=server 
+
+        public static string defaultFilePath = "FILES";
+        private long defaultFileSizeInBytes = 1024 * 1024 * 50;
+        private readonly IWebHostEnvironment _environment;
+        public FileUploadService(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+
+            string contentRootPath = _environment.ContentRootPath;
+
+            //ClientHelper.BaseDBsPath = contentRootPath + "\\" + defaultDBsPath + "\\";
+            //ClientHelper.BaseFilesPath = contentRootPath + "\\" + defaultFilePath + "\\";
+        }
+
+        public FileNotificationService NotificationService { get; } = new FileNotificationService();
+        public string GetContentRootPath()
+        {
+            return _environment.ContentRootPath;
+        }
+        public async Task UploadAsync(Microsoft.AspNetCore.Components.Forms.IBrowserFile file)
+        {
+            try
+            {
+                // Dateigröße 
+                if (file.Size >= defaultFileSizeInBytes)
+                    return;
+
+                string contentRootPath = _environment.ContentRootPath;
+
+                Guid itemGUID = Guid.NewGuid();
+                string relativeSaveFilePath =
+                    contentRootPath.ToString() + "\\" +
+                    defaultFilePath.ToString();
+
+                // Verzeichnis 
+                if (!Directory.Exists(relativeSaveFilePath))
+                {
+                    Directory.CreateDirectory(relativeSaveFilePath);
+                }
+                if (!Directory.Exists(relativeSaveFilePath))
+                    return;
+
+                // Voller Dateipfad 
+                var extension = Helper.SplitGetLast(file.Name);
+                var fullFilePath = Path.Combine(relativeSaveFilePath, itemGUID.ToString() + "." + extension);
+
+                // Write DOWN 
+                await using FileStream fs = new(fullFilePath, FileMode.Create);
+                Task f = file.OpenReadStream(defaultFileSizeInBytes).CopyToAsync(fs);
+                await f;
+
+                // Notification 
+                FileNotification token = new FileNotification();
+                token.FileGUID = itemGUID;
+                token.FullFilePath = fullFilePath;
+                token.OriginalFileName = file.Name;
+                NotificationService.NotifySync(token);
+
+                await Task.FromResult<string>("OK");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("FAIL: " + ex.Message);
+            }
+        }
+
+
+    }
+
+    public interface IFileDragDropService
+    {
+        string GetContentRootPath();
+        Task UploadAsync(Microsoft.AspNetCore.Components.Forms.IBrowserFile file);
+        FileNotificationService NotificationService { get; }
+    }
+
+    public class FileDragDropUploadService : IFileDragDropService
+    {
+        // https://docs.microsoft.com/de-de/aspnet/core/blazor/file-uploads?view=aspnetcore-6.0&pivots=server 
+        public static string defaultFilePath = "FILES";
+        private long defaultFileSizeInBytes = 1024 * 1024 * 50;
+        private readonly IWebHostEnvironment _environment;
+        public FileDragDropUploadService(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+
+            string contentRootPath = _environment.ContentRootPath;
+
+            //ClientHelper.BaseDBsPath = contentRootPath + "\\" + defaultDBsPath + "\\";
+            //ClientHelper.BaseFilesPath = contentRootPath + "\\" + defaultFilePath + "\\";
+        }
+        // Properties 
+        public FileNotificationService NotificationService { get; } = new FileNotificationService();
+        public string GetContentRootPath()
+        {
+            return _environment.ContentRootPath;
+        }
+        public async Task UploadAsync(Microsoft.AspNetCore.Components.Forms.IBrowserFile file)
+        {
+            try
+            {
+                // Dateigröße 
+                if (file.Size >= defaultFileSizeInBytes)
+                    return;
+
+                string contentRootPath = _environment.ContentRootPath;
+
+                Guid itemGUID = Guid.NewGuid();
+                string relativeSaveFilePath =
+                    contentRootPath.ToString() + "\\" +
+                    defaultFilePath.ToString();
+
+                // Verzeichnis 
+                if (!Directory.Exists(relativeSaveFilePath))
+                {
+                    Directory.CreateDirectory(relativeSaveFilePath);
+                }
+                if (!Directory.Exists(relativeSaveFilePath))
+                    return;
+
+                // Voller Dateipfad 
+                var extension = Helper.SplitGetLast(file.Name);
+                var fullFilePath = Path.Combine(relativeSaveFilePath, itemGUID.ToString() + "." + extension);
+
+                // Write DOWN 
+                await using FileStream fs = new(fullFilePath, FileMode.Create);
+                Task f = file.OpenReadStream(defaultFileSizeInBytes).CopyToAsync(fs);
+                await f;
+
+                // Notification 
+                FileNotification token = new FileNotification();
+                token.FileGUID = itemGUID;
+                token.FullFilePath = fullFilePath;
+                token.OriginalFileName = file.Name;
+                NotificationService.NotifySync(token);
+
+                // Feedback 
+                await Task.FromResult<string>("OK");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("FAIL: " + ex.Message);
+            }
+        }
+
+
+    }
+
 
     public class Entry : IEntry
     {
--- ELEMENTs.Environment/Startup.cs
+++ ELEMENTs.Environment/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ELEMENTs.Environment.Data;
 using ELEMENTS;
+using ELEMENTS.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
