8b6d8ac039bb4575c61d3acb63d94c5f7f1fe876
JP <juergenpott@googlemail.com>
2022-04-20 19:32:51 +0200

    - Toast added - Popup added - example pages updated

---
+++ ELEMENTS.Controls/Popup/MenuPopup.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELEMENTS
+{
+    
+
+    public class MenuEntry
+    {
+        public string Link { get; set; } = "/";
+        public string Title { get; set; } = "";
+    }
+}
---
+++ ELEMENTS.Controls/wwwroot/toast.js
@@ -0,0 +1,70 @@
+﻿
+export function initShow(btnID, toastID, dotnethelper)
+{
+    try
+    {
+        var btn = document.getElementById(btnID);
+        if (btn === null) { return; }
+
+        var element = document.getElementById(toastID);
+        if (element === null) { return; }
+
+        // Create toast instance
+        var myToast = new bootstrap.Toast(element, {
+            autohide: true
+        });
+
+        btn.addEventListener("click", function () {
+            myToast.show();
+        });
+    }
+    catch (e) {
+        alert(e);
+    }
+}
+export function showToast(toastID) {
+    try {
+
+        var element = document.getElementById(toastID);
+        if (element === null) { return; }
+
+        // Create toast instance
+        var myToast = new bootstrap.Toast(element, {
+            autohide: true
+        });
+
+        if (myToast !== null) { myToast.show(); }
+
+    }
+    catch (e) {
+        alert(e);
+    }
+}
+
+export function showToastWithMessage(toastID, title, titleID, message, messageID) {
+    try {
+
+        var element = document.getElementById(toastID);
+        if (element === null) { return; }
+
+        var titleELEMENT = document.getElementById(titleID);
+        if (titleELEMENT === null) { return; }
+        titleELEMENT.innerText = title;
+
+        var msgELEMENT = document.getElementById(messageID);
+        if (msgELEMENT === null) { return; }
+        msgELEMENT.innerText = message;
+
+        // Create toast instance
+        var myToast = new bootstrap.Toast(element, {
+            autohide: true
+        });
+
+        if (myToast !== null) { myToast.show(); }
+
+    }
+    catch (e) {
+        alert(e);
+    }
+}
+
