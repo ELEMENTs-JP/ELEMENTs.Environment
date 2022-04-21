4fe227476ae8c8eb38c9136c5a4d3c61321376ba
JP <juergenpott@googlemail.com>
2022-04-21 08:17:19 +0200

    - Breadcrumbpath added - example pages updated

---
+++ ELEMENTS.Controls/Navigation/Classes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELEMENTS
+{
+    public class BreadCrumbPathElement
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+    }
+}
