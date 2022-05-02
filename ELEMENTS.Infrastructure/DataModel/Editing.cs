6aee56a1e57b87666df9c600f025f083c7320010
JP <juergenpott@googlemail.com>
2022-05-02 10:21:13 +0200

    - Edit ui startet (not finished) a lot is missing to this.

---
+++ ELEMENTS.Infrastructure/DataModel/Editing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELEMENTS.Infrastructure
+{
+    public class EditField
+    {
+        public string Title { get; set; } = string.Empty;
+        public string ColumnCSSClass { get; set; } = "col";
+        
+    }
+}
